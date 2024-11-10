cb438318fec6c4bb3618d5c217f77c54d4c16d05
Nishant Dakchand Punde <pundenishant@gmail.com>
2024-11-10 18:33:29 +0530

    c

--- models/admin_login.js
+++ models/admin_login.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
 const adminSchema = new mongoose.Schema({
+    restaurant: {
+        type: String,
+        required: true,
+    },
     username: {
         type: String,
         required: true,
